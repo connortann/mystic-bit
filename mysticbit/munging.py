e56990795ea16ff8fdead183af0c468226413bd8
ConnorTann <connor_tann@hotmail.com>
2018-11-24 10:21:03 +0000

    Data munging functions

--- mysticbit/munging.py
+++ mysticbit/munging.py
@@ -2,4 +2,41 @@
 
 import pandas as pd
 
-pass
+
+def load_log_data():
+    """ Return pandas dataframe of log data """
+    df = (pd.read_csv('../data/HACKA_DS.csv', sep=';')
+          .rename(columns=lambda x: x.strip())
+          )
+    return df
+
+
+def create_ml_dataframe(df, feature_cols=['GR'], feature_lags=[1, 2, 3],
+                        label_cols=['GR'], label_lags=[5, 10]):
+    """ Create dataframe with 'features' and 'labels', from the raw log dataframe """
+
+    cols_to_keep = list(set(['TVDSS', 'HACKANAME', 'RES_ID'] + feature_cols + label_cols))
+
+    # Resample at 1m
+    # TODO: resampling that handles RESID
+    df_ml = df.copy()
+    df_ml = (df_ml[cols_to_keep]
+             .astype({"TVDSS": int})
+             .groupby(['HACKANAME', 'TVDSS'])
+             .mean())
+
+    # Feature lagging
+    for col in feature_cols:
+        for lag in feature_lags:
+            kwargs = {col + '_' + str(lag): lambda x: x[col].shift(lag)}
+            df_ml = df_ml.assign(**kwargs)
+
+    # Label lagging
+    for col in label_cols:
+        for lag in label_lags:
+            kwargs = {'futr_' + col + '_' + str(lag): lambda x: x[col].shift(-lag)}
+            df_ml = df_ml.assign(**kwargs)
+
+    df_ml = df_ml.dropna()
+
+    return df_ml

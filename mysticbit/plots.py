c0c9dd7e129800b6a89023d7ca9953ace4268e54
ConnorTann <connor_tann@hotmail.com>
2018-11-24 12:24:02 +0000

    Log plot function

--- mysticbit/plots.py
+++ mysticbit/plots.py
@@ -22,47 +22,40 @@ def plot_well_map(df_logs, fig_size=(10, 10)):
     return f, ax
 
 
-def make_log_plot(well_logs, strata, well_index, ztop=None, zbot=None, fig_size=(8,12)):
+def make_log_plot(df_logs, well_name, cols=['GR', 'DT', 'CALI'], ztop=None, zbot=None, fig_size=(8, 12)):
     """ Single well log plot, both GR and Resistivity """
 
-    logs = well_logs[well_logs['WELL_INDEX'] == well_index]
-    logs = logs.sort_values(by='DEPT')
+    logs = df_logs[df_logs['HACKANAME'] == well_name]
+    logs = logs.sort_values(by='TVDSS')
 
     if not ztop:
-        ztop = logs.DEPT.min()
+        ztop = logs.TVDSS.min()
     if not zbot:
-        zbot = logs.DEPT.max()
+        zbot = logs.TVDSS.max()
 
-    f, ax = plt.subplots(nrows=1, ncols=2, figsize=fig_size)
-    ax[0].plot(logs.GR, logs.DEPT, '-g')
-    ax[1].plot(logs.RESD, logs.DEPT, '-')
+    f, ax = plt.subplots(nrows=1, ncols=len(cols), figsize=fig_size)
 
     for i in range(len(ax)):
+        log_name = cols[i]
+        ax[i].scatter(logs[log_name], logs['TVDSS'], marker='+')
+        ax[i].set_xlabel(log_name)
         ax[i].set_ylim(ztop, zbot)
         ax[i].invert_yaxis()
         ax[i].grid()
         ax[i].locator_params(axis='x', nbins=3)
 
-    ax[0].set_xlabel("GR")
-    ax[0].set_xlim(0, 150)
-    ax[1].set_xlabel("RESD")
-    ax[1].set_xscale('log')
-    ax[1].set_xlim(0.2, 2000)
+        if i > 0:
+            ax[i].set_yticklabels([])
 
-    ax[1].set_yticklabels([])
+    # ax[0].set_xlabel("GR")
+    # ax[0].set_xlim(0, 150)
+    # ax[1].set_xlabel("RESD")
+    # ax[1].set_xscale('log')
+    # ax[1].set_xlim(0.2, 2000)
 
-    f.suptitle('Well: %s' % logs.iloc[0]['WELL_INDEX'], fontsize=14, y=0.94)
+    # ax[1].set_yticklabels([])
 
-    # Plot strata picks
-    cdict = get_colours(strata)
-    well_strata = strata.loc[well_index][3:]
-    for zone, depth in well_strata.iteritems():
-        if ztop < depth < zbot:
-            for i in range(len(ax)):
-                ax[i].axhline(y=depth, color=cdict[zone])
+    f.suptitle('Well: {}'.format(well_name), fontsize=14, y=0.94)
 
-            # Labels on rightmost axis
-            pos_x = ax[-1].get_xlim()[1]
-            ax[-1].text(pos_x, depth, zone)
-    return ax
+    return f, ax
 
